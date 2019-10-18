f4fa0970e0c188d6e7feb83459cee42f3ea318d8
Lewis Sparlin <lewis@edatasource.com>
2019-10-18 15:26:52 -0700

    updated link_dotfiles

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ls -l1 ~/dotfiles | grep -Ev "backup|link_dotfiles|plugins" | xargs -I% ln -vhs ~/dotfiles/% ~/.%
-ln -hs ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf 
-ln -hs ~/dotfiles/omz-plugins/zsh-autosuggestions ~/dotfiles/oh-my-zsh/custom/plugins/zsh-autosuggestions
-ln -hs ~/dotfiles/omz-plugins/zsh-history-substring-search ~/dotfiles/oh-my-zsh/custom/plugins/zsh-history-substring-search
+ln -vhs ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf 
+ln -vhs ~/dotfiles/omz-plugins/zsh-autosuggestions ~/dotfiles/oh-my-zsh/custom/plugins/zsh-autosuggestions
+ln -vhs ~/dotfiles/omz-plugins/zsh-history-substring-search ~/dotfiles/oh-my-zsh/custom/plugins/zsh-history-substring-search
 mkdir ~/dotfiles/oh-my-zsh/custom/themes
-ln -hs ~/dotfiles/omz-plugins/spaceship-prompt/spaceship-prompt.zsh-theme ~/dotfiles/oh-my-zsh/custom/themes/spaceship.zsh-theme
+ln -vhs ~/dotfiles/omz-plugins/spaceship-prompt/spaceship-prompt.zsh ~/dotfiles/oh-my-zsh/custom/themes/spaceship.zsh-theme
