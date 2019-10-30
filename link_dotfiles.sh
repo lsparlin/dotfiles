13cd5fd6f3f572a5f88ec0303fc23f0d5584cfac
Lewis Sparlin <lewis@edatasource.com>
2019-10-30 13:18:59 -0500

    Fixed link_dotfiles script

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -6,4 +6,4 @@ ls -l1 $MY_DOTFILES_DIR |\
   grep -Ev "backup|link_dotfiles|custom" |\
   xargs -I% ln -vhs $MY_DOTFILES_DIR/% $HOME/.%
 ln -vhs $MY_DOTFILES_DIR/tmux/.tmux.conf $HOME/.tmux.conf 
-ln -vhs $MY_DOTFILES_DIR/zsh-custom/themes/spaceship-prompt/spaceship.zsh $MY_DOTFILES_DIR/zsh-custom/themes/
+ln -vhs $MY_DOTFILES_DIR/zsh-custom/themes/spaceship-prompt/spaceship.zsh $MY_DOTFILES_DIR/zsh-custom/themes/spaceship.zsh-theme
