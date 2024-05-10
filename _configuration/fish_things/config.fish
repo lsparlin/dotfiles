b33db401fb6976bffff49cb9a67af4ec0af9e300
Lewis Sparlin <lewis.sparlin@ext.chime.com>
2024-05-10 14:53:19 -0500

    Add chime only dotfiles config changes

--- _configuration/fish_things/config.fish
+++ _configuration/fish_things/config.fish
@@ -12,3 +12,6 @@ which fzf >/dev/null && fzf --fish | source || echo "INFO: fzf not installed"
 # ASDF gets deprioritized by some environments like tmux
 # Also this seems to replace the commented out asdf setup script above
 fish_add_path -m $HOME/.asdf/shims
+
+# DO NOT COMMIT (Chime ONLY)
+source ~/.chime_vars.fish
