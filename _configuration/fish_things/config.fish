e628b2f830176cbf3591c9e7fdbc0846e9fe4927
Lewis Sparlin <lewis.sparlin@ext.chime.com>
2024-05-10 14:53:19 -0500

    Add chime only dotfiles config changes

--- _configuration/fish_things/config.fish
+++ _configuration/fish_things/config.fish
@@ -6,13 +6,7 @@ end
 # setup fzf keybindings (per homebrew instructions)
 which fzf >/dev/null && fzf --fish | source || echo "INFO: fzf not installed"
 
-# This does not seem to work in all environments
-# source /usr/local/opt/asdf/libexec/asdf.fish
-
-# ASDF gets deprioritized by some environments like tmux
-# Also this seems to replace the commented out asdf setup script above
-# fish_add_path -m $HOME/.asdf/shims
-# ASDF configuration code
+# ASDF configuration code as recommended by asdf docs
 if test -z $asdf_data_dir
     set _asdf_shims "$HOME/.asdf/shims"
 else
@@ -25,3 +19,6 @@ if not contains $_asdf_shims $PATH
     set -gx --prepend PATH $_asdf_shims
 end
 set --erase _asdf_shims
+
+# Chime ONLY
+source ~/.chime_vars.fish
