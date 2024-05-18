1b73cca661f89e4c9df1e933ef42c6ab44afa04d
Lewis Sparlin <hello@lewismsparlin.com>
2024-05-18 13:23:19 -0500

    Revert "Remove environment-specific files, too-specific asdf initializer"
    This reverts commit 0fd61c8a8be024eecf398f8aa40166f7ef7f59f8.

--- _configuration/fish_things/config.fish
+++ _configuration/fish_things/config.fish
@@ -3,10 +3,3 @@ if status is-interactive
 end
 
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
-fzf_configure_bindings --directory=\cf
-
-# This does not seem to work anymore
-# source /usr/local/opt/asdf/libexec/asdf.fish
-
-# ASDF gets deprioritized by some environments like tmux
-fish_add_path -m $HOME/.asdf/shims
