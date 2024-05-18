67cc2e2b2907eeea4185ede654c5d65258b7a2da
Lewis Sparlin <hello@lewismsparlin.com>
2024-05-18 13:28:54 -0500

    Replace too-specific asdf initializer, some environment updated

--- _configuration/fish_things/config.fish
+++ _configuration/fish_things/config.fish
@@ -2,4 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+fzf_configure_bindings --directory=\cf
+
+# This does not seem to work in all environments
+# source /usr/local/opt/asdf/libexec/asdf.fish
+
+# ASDF gets deprioritized by some environments like tmux
+# Also this seems to replace the commented out asdf setup script above
+fish_add_path -m $HOME/.asdf/shims
