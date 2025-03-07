105131280115916f547cc002e717c37b1e786b1f
Lewis Sparlin <hello@lewismsparlin.com>
2025-03-07 15:42:10 -0600

    add helix link to link_dotfiles script, fish asdf update

--- _configuration/fish_things/config.fish
+++ _configuration/fish_things/config.fish
@@ -11,4 +11,17 @@ which fzf >/dev/null && fzf --fish | source || echo "INFO: fzf not installed"
 
 # ASDF gets deprioritized by some environments like tmux
 # Also this seems to replace the commented out asdf setup script above
-fish_add_path -m $HOME/.asdf/shims
+# fish_add_path -m $HOME/.asdf/shims
+# ASDF configuration code
+if test -z $asdf_data_dir
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$asdf_data_dir/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
